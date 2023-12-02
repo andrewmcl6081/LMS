41ac6e81b7d1d339bafe84012da788223add9eff
haydencj <haydenjohns02@gmail.com>
2023-12-01 18:05:16 -0600

    home page edit

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 import sqlite3
 
 app = Flask(__name__)
 
 DATABASE = "LMS.db"
 
-@app.route('/api/data', methods=['GET'])
+@app.route('/', methods=['GET'])
 def get_data():
     try:
         sqliteConnection = sqlite3.connect('LMS.db')
@@ -15,13 +15,13 @@ def get_data():
         sqlite_select_query = "SELECT * FROM BOOK;"
         cursor.execute(sqlite_select_query)
         data = cursor.fetchall()
-        
         cursor.close()
-        return jsonify({ "data": data})
     
+        
     except Exception as e:
         print("Error while connecting to sqlite", e)
 
+    return render_template('books.html', books=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
