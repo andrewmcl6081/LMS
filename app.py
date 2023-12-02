ba8e18742cadd0212364f1be5bd21c73b4a3e5c9
haydencj <haydenjohns02@gmail.com>
2023-12-01 19:53:11 -0600

    task2 q1 completed

--- app.py
+++ app.py
@@ -6,22 +6,51 @@ app = Flask(__name__)
 DATABASE = "LMS.db"
 
 @app.route('/', methods=['GET'])
-def get_data():
+def home():
     try:
         sqliteConnection = sqlite3.connect('LMS.db')
         cursor = sqliteConnection.cursor()
-        print("Database successfully connected to sqlite")
+        print("Database successfully connected: INITIAL")
         
-        sqlite_select_query = "SELECT * FROM BOOK;"
+        sqlite_select_query = "SELECT * FROM User_View;"
         cursor.execute(sqlite_select_query)
+
         data = cursor.fetchall()
         cursor.close()
-    
+        sqliteConnection.close()
         
     except Exception as e:
         print("Error while connecting to sqlite", e)
 
     return render_template('books.html', books=data)
 
+@app.route('/checkout/', methods=['POST'])
+def checkout():
+    bookid = request.form.get('bookid')
+    cardno = request.form.get('cardno')
+    branchid = request.form.get('branchid')
+
+    try:
+        sqliteConnection = sqlite3.connect('LMS.db')
+        cursor = sqliteConnection.cursor()
+        print("Database successfully connected: CHECKOUT")
+        
+        query = """
+                INSERT INTO Book_Loans(book_id, branch_id, card_no, date_out, due_date)
+                VALUES(?, ?, ?, CURRENT_DATE, date('now', '+30 days'));
+            """
+        
+        cursor.execute(query, (bookid, branchid, cardno))
+        sqliteConnection.commit()
+
+        cursor.close()
+        sqliteConnection.close()
+    
+    except Exception as e:
+        print("Error while connecting to sqlite", e)
+
+    return redirect(url_for('home'))
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=3000)
